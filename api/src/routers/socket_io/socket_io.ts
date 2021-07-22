982858706e8fe4c9af8534afbe2d6cc998bde914
Azam <pabek92@gmail.com>
2021-07-22 10:46:48 +0200

    add jwt-socket.oi

--- api/src/routers/socket_io/socket_io.ts
+++ api/src/routers/socket_io/socket_io.ts
@@ -1,6 +1,6 @@
 import { Server, Socket } from "socket.io"
 import { refresh_token } from "../../controlles/auth/socket_io/refresh_token"
-
+import { passport_io } from "../../midlleware/soket_io/passport_io"
 export const socket_io = (http: any) => {
   const io = new Server(http, {
     cors: {
@@ -10,6 +10,7 @@ export const socket_io = (http: any) => {
   return async () => {
     try {
       console.log("Socket is connection")
+      io.use(passport_io)
       io.on("connect", async (socket: Socket) => {
         await refresh_token(socket, io)
       })
