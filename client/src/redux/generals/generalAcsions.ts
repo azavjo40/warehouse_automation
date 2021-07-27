b0d604f5f390091b4f7b72db68638a8144014bd6
Azam <pabek92@gmail.com>
2021-07-27 20:20:18 +0200

    add ecrypt

--- client/src/redux/generals/generalAcsions.ts
+++ client/src/redux/generals/generalAcsions.ts
@@ -1,6 +1,9 @@
 import { IActionGenerals, IClearForm } from "../../interface/generals"
 import { IS_LOADING, SHOW_ALERT, CLEAR_FORM } from "./types"
 import { Dispatch } from "redux"
+import { useHttp } from "../hooks/useHttp"
+import { getStorage } from "../../utils/storage"
+import NodeRSA from "node-rsa"
 
 export const showLoader = (bool: boolean) => {
   return (dispatch: Dispatch<IActionGenerals>): void => {
@@ -35,6 +38,46 @@ export function showAlert(text: string) {
           payload: null,
         })
       }, 3000)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+
+export const autoCreateKey = () => {
+  return async (dispatch: Dispatch<IClearForm>) => {
+    try {
+      const storage = getStorage()
+      const secretKey = JSON.parse(localStorage.getItem("secret_key") as any)
+      if (secretKey) {
+        return { secretKey }
+      } else {
+        const userId = storage.data.userId
+        const options: any = {
+          url: "/api/auto/create/key",
+          method: "POST",
+          body: userId,
+          file: null,
+          token: storage.data.token,
+          type: null,
+        }
+        const { data } = await dispatch(useHttp(options))
+        if (data.publicK) {
+          const key = new NodeRSA({ b: 1024 })
+          const public_key = key.exportKey("public")
+          const private_key = key.exportKey("private")
+          const publicK = new NodeRSA(public_key)
+          const privateK = new NodeRSA(private_key)
+
+          localStorage.setItem(
+            "secret_key",
+            JSON.stringify({ publicK, privateK })
+          )
+          const secretKey = { publicK, privateK }
+
+          return { secretKey }
+        }
+      }
     } catch (e) {
       console.log(e)
     }
