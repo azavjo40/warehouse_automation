82058eaadb1ac9c57316d3fbf9ff5d30d002dbb3
Azam <pabek92@gmail.com>
2021-07-30 11:39:08 +0200

    add code

--- api/src/controlles/generals/generals.ts
+++ api/src/controlles/generals/generals.ts
@@ -5,44 +5,35 @@ import NodeRSA from "node-rsa"
 export const autoCreateCryptoKey = async (req: Request, res: Response) => {
   try {
     const { userId, clientKey, timeId } = req.body
+    console.log(userId, clientKey, timeId)
     const keyOnServer: any = await SecretCryptoKey.findOne({
       userId: userId ? userId : timeId,
     })
-
-    if (clientKey && keyOnServer) {
-      const serverDecryptKey = new NodeRSA(keyOnServer.privateKey)
-
-      const keyDecryptClient: any = JSON.parse(
-        serverDecryptKey.decrypt(clientKey, "utf8")
-      )
-
-      const keyOnServerTOString: string = JSON.stringify(keyOnServer)
-
-      const encrypt = new NodeRSA(keyDecryptClient)
-
-      const encryptData = encrypt.encrypt(keyOnServerTOString, "base64")
-
-      res.status(200).json(encryptData)
-    } else if (keyOnServer) {
-      res.status(200).json({ publicKey: keyOnServer.publicKey })
+    console.log(keyOnServer)
+    if (keyOnServer) {
+      const encrypt = new NodeRSA(clientKey)
+      const keyOnServerToString: string = JSON.stringify(keyOnServer)
+      const dataEncrypt: any = encrypt.encrypt(keyOnServerToString, "base64")
+      res.status(200).json(dataEncrypt)
     } else {
       const newKey = new NodeRSA({ b: 1024 })
-
       const publicKey = newKey.exportKey("public")
-
       const privateKey = newKey.exportKey("private")
 
-      await new SecretCryptoKey({
+      const secretCryptoKey = await new SecretCryptoKey({
         privateKey,
         publicKey,
         userId: userId ? userId : timeId,
       }).save()
 
+      const encrypt = new NodeRSA(clientKey)
+      const keyOnServerToString: string = JSON.stringify(secretCryptoKey)
+      const dataEncrypt: any = encrypt.encrypt(keyOnServerToString, "base64")
       if (!userId) {
         setTimeout(() => SecretCryptoKey.deleteOne({ userId: timeId }), 9000)
       }
 
-      res.status(201).json({ publicKey })
+      res.status(201).json(dataEncrypt)
     }
   } catch (e) {
     console.log(e)
